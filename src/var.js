02147b68d811c5300abc5e182c67088e62783f05
혀느현스 <hyuns@hyuns.dev>
2021-12-17 20:19:18 +0900

    var List

--- src/var.js
+++ src/var.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const { User, Var } = require("./models/schema")
 
 let connection = null
@@ -19,28 +20,51 @@ exports.varCreate = async (event) => {
     if (!event?.headers?.Authorization) return createResponse(400, { message: "need_token" })
     const token = event.headers.Authorization.replace("Bearer ", "")
 
+    if (event.pathParameters.isSecret !== "secret" && event.pathParameters.isSecret !== "global") return createResponse(404, "")
+    const isSecret = event.pathParameters.isSecret == "secret"
+
     try {
         jwt.verify(token, process.env.MASTER_PASSWORD)
     } catch (err) {
         return createResponse(403, { message: "wrong_token" })
     }
 
-    const { group, key, value, isSecret, img } = JSON.parse(event.body)
-    if (!group || !key || !value || !img || !isSecret) return createResponse(400, {message: "need_more_info"})
+    const { group, key, value, img } = JSON.parse(event.body)
+    if (!group || !key || !value || !img) return createResponse(400, {message: "need_more_info"})
 
     await connect()
     
-    const alreadyVar = await findOne({varGroup: group, varKey: key})
+    const alreadyVar = await Var.findOne({varGroup: group, varKey: key, varIsSecret: isSecret})
     if (alreadyVar) return createResponse(202, {message: "already_var_exist"})
 
     const variableInfo = new Var({
         varGroup: group,
         varKey: key,
         varValue: value,
-        varIsSecret: isSecret === "true" ? true : false,
+        varIsSecret: isSecret,
         varImg: img
     })
 
     const variable = await variableInfo.save()
     return createResponse(200, {message: "var_created", data: variable})
+}
+
+exports.varList = async (event) => {
+    if (!event?.headers?.Authorization) return createResponse(400, { message: "need_token" })
+    const token = event.headers.Authorization.replace("Bearer ", "")
+
+    if (event.pathParameters.isSecret !== "secret" && event.pathParameters.isSecret !== "global") return createResponse(404, "")
+    const isSecret = event.pathParameters.isSecret == "secret"
+
+    try {
+        jwt.verify(token, process.env.MASTER_PASSWORD)
+    } catch (err) {
+        return createResponse(403, { message: "wrong_token" })
+    }
+
+    await connect()
+    
+    const variable = await Var.find({varIsSecret: isSecret})
+
+    return createResponse(200, {message: "vars_found", data: variable})
 }
