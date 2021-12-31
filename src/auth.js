4d63d7caee754fb41969c3ed365009c28b2d133b
혀느현스 <hyuns@hyuns.dev>
2021-12-31 14:39:29 +0900

    cors 허용 설정

--- src/auth.js
+++ src/auth.js
@@ -14,7 +14,13 @@ const connect = async () => {
 
 const createResponse = (status, body) => ({
     statusCode: status,
-    body: JSON.stringify(body)
+    body: JSON.stringify(body),
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        'Access-Control-Allow-Credentials': false,
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods":"OPTIONS,POST,GET,PUT,DELETE"
+    }
 })
 
 exports.createAccount = async (event) => {
